11d3e3924c1991d18193ce99503f05eab81337f1
Nitin Pawar <107263213+nrp033@users.noreply.github.com>
2022-12-02 08:00:52 +0530

    Checking if Employee is present or Absent Using Math.Random() and let and const datatypes

---
+++ EmployeeWage-Jscript_And_Node/EmployeeWage.js
@@ -0,0 +1,8 @@
+const IS_Absent=0;
+let empCheck=Math.floor(Math.random()*10)%2;
+if(empCheck==IS_Absent){
+    console.log("\nEmployee is Present !");
+}
+else{
+    console.log("Employee is Absent !");
+}
