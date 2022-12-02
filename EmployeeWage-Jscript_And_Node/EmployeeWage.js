da2cfe0a8b3fa4efe4070adae4fc97d1e2d7735a
Nitin Pawar <107263213+nrp033@users.noreply.github.com>
2022-12-02 08:32:30 +0530

    Calculating Employee wage for Month using For loop

--- EmployeeWage-Jscript_And_Node/EmployeeWage.js
+++ EmployeeWage-Jscript_And_Node/EmployeeWage.js
@@ -1,17 +1,16 @@
 const IS_FullTime=1;
 const IS_PartTime=2;
-const Part_TimeHours=4;
-const Full_TimeHours=8;
 const WagePer_Hour=20;
+const MaxWorkingDays_Month=20;
 
 function GetWorkingHrs(empCheck){
     switch(empCheck){
 
         case IS_FullTime:
-            return Full_TimeHours;
+            return 8;
             
         case IS_PartTime:
-            return Part_TimeHours;
+            return 4;
         
             default:
             return 0;
@@ -19,11 +18,13 @@ function GetWorkingHrs(empCheck){
     }
 }
 
-
+    let TotalHrs=0;
+    for(let day=0;day<MaxWorkingDays_Month;day++){
         let empCheck=Math.floor(Math.random()*10)%3;
-        let empHours=GetWorkingHrs(empCheck);
-        let EmpWage=empHours*WagePer_Hour;
-        console.log("Working Hours  : "+empHours+"  Employee Wage : " +EmpWage);
+        TotalHrs+=GetWorkingHrs(empCheck);
+    }
+    let EmpWage=TotalHrs*WagePer_Hour;
+    console.log("Hours  : "+TotalHrs+"  Employee Wage : " +EmpWage);
    
     
 
