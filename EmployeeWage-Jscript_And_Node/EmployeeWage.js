91ca6f16128c5da118e0b674e20fe84b8f723582
Nitin Pawar <107263213+nrp033@users.noreply.github.com>
2022-12-02 08:13:41 +0530

    Calculating the emp wage if employee is FullTime, PartTime or absent based on working hours

--- EmployeeWage-Jscript_And_Node/EmployeeWage.js
+++ EmployeeWage-Jscript_And_Node/EmployeeWage.js
@@ -1,8 +1,29 @@
-const IS_Absent=0;
-let empCheck=Math.floor(Math.random()*10)%2;
-if(empCheck==IS_Absent){
-    console.log("\nEmployee is Present !");
-}
-else{
-    console.log("Employee is Absent !");
-}
+const IS_FullTime=1;
+const IS_PartTime=2;
+const WagePer_Hour=20;
+
+
+    let empHours=0;
+    let empCheck=Math.floor(Math.random()*10) % 3;
+    switch(empCheck){
+
+        case IS_FullTime:
+            empHours= 8;
+            break;
+        case IS_PartTime:
+            empHours= 4;
+            break;
+        default:
+            empHours=0;
+            break; 
+            
+    }
+
+    let EmpWage=empHours*WagePer_Hour;
+    console.log("Working Hours  : "+empHours+"  Employee Wage : " +EmpWage);
+   
+    
+
+
+
+
